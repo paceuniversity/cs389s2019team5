34fadc038a2053f9a67b8fc7873d40247e0616d7
Raami Sharif <raamisharif@gmail.com>
2019-03-27 11:42:36 -0400

    added student activity and firebase

---
+++ EzAttend/app/src/main/java/edu/pace/cs389s2019team5/ez_attend/StudentActivity.java
@@ -0,0 +1,13 @@
+package edu.pace.cs389s2019team5.ez_attend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student);
+    }
+}
