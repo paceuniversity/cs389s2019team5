257df24559def924c563b4a333088e165d9f9d79
Edward Gervis <edward.gervis@gmail.com>
2019-03-30 01:49:04 -0400

    main menu formatting

--- EzAttend/app/src/main/java/edu/pace/cs389s2019team5/ez_attend/TeacherActivity.java
+++ EzAttend/app/src/main/java/edu/pace/cs389s2019team5/ez_attend/TeacherActivity.java
@@ -1,6 +1,5 @@
 package edu.pace.cs389s2019team5.ez_attend;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,17 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class TeacherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void openTeacherActivity(View v){
-        Intent i = new Intent(MainActivity.this, TeacherActivity.class);
-        startActivity(i);
+        setContentView(R.layout.activity_teacher);
     }
 
 }
