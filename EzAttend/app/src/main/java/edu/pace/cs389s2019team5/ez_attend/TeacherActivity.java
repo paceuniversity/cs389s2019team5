11a4634b6f9b1e0c0a899f471ef54d56253acf17
Ian-Groombridge <igroombridge2010@gmail.com>
2019-04-05 15:35:52 -0400

    added teacher login

--- EzAttend/app/src/main/java/edu/pace/cs389s2019team5/ez_attend/TeacherActivity.java
+++ EzAttend/app/src/main/java/edu/pace/cs389s2019team5/ez_attend/TeacherActivity.java
@@ -1,11 +1,18 @@
 package edu.pace.cs389s2019team5.ez_attend;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import edu.pace.cs389s2019team5.ez_attend.Firebase.ClassSession;
+import edu.pace.cs389s2019team5.ez_attend.Firebase.Controller;
 
 public class TeacherActivity extends AppCompatActivity {
 
@@ -15,4 +22,10 @@ public class TeacherActivity extends AppCompatActivity {
         setContentView(R.layout.activity_teacher);
     }
 
+    public void launchAttendance (View view) {
+        Controller session = new Controller();
+        OnSuccessListener<ClassSession> success = null;
+        OnFailureListener failure = null;
+        session.beginClassSession(success, failure);
+    }
 }
