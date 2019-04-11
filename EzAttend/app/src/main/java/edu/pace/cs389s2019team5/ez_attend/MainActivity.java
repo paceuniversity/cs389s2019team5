50b6dc753628f8fa3c06c393ad52cea19426cfb3
Raami Sharif <raamisharif@gmail.com>
2019-04-11 02:42:24 -0400

    added firebase ui auth and firestore

--- EzAttend/app/src/main/java/edu/pace/cs389s2019team5/ez_attend/MainActivity.java
+++ EzAttend/app/src/main/java/edu/pace/cs389s2019team5/ez_attend/MainActivity.java
@@ -3,14 +3,67 @@ package edu.pace.cs389s2019team5.ez_attend;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+    // Choose an arbitrary request code value
+    private static final int RC_SIGN_IN = 773;
+    private static final String TAG = MainActivity.class.getName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+
+        } else {
+            startActivityForResult(
+                    // Get an instance of AuthUI based on the default app
+                    AuthUI.getInstance().createSignInIntentBuilder()
+                            .setAvailableProviders(Arrays.asList(
+                                    new AuthUI.IdpConfig.EmailBuilder().build()))
+                            .build(),
+                    RC_SIGN_IN);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == RESULT_OK) {
+                Log.i(TAG, "Successfully logged in");
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    Log.d(TAG, "User did not sign in");
+                    return;
+                }
+
+                if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Log.d(TAG, "No internet connection");
+                    return;
+                }
+
+                Log.e(TAG, "Sign-in error: ", response.getError());
+            }
+        }
     }
 
     public void launchStudent(View view) {
