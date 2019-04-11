161c7d5fcc9400f70d23ffbd87ea94bc42b0d275
Edward Gervis <edward.gervis@gmail.com>
2019-04-10 21:24:39 -0400

    created the new main activity

---
+++ EzAttend/app/src/main/java/edu/pace/cs389s2019team5/ez_attend/StudentFragment.java
@@ -0,0 +1,31 @@
+package edu.pace.cs389s2019team5.ez_attend;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class StudentFragment extends Fragment {
+
+
+    public StudentFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_student, container, false);
+    }
+
+    public void openClass(String classID) {
+        //selecting a class will open a new fragment such as: new StudentClassFragment(String classID)
+
+        //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_content, new StudentClassFragment(classID)).commit();
+    }
+
+}
