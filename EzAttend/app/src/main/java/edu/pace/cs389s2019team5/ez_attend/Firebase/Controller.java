20ec321b078555c62cb8eacdb6152bc6f3217fa0
Edward Gervis <edward.gervis@gmail.com>
2019-04-24 15:13:10 -0400

    added method stubs

--- EzAttend/app/src/main/java/edu/pace/cs389s2019team5/ez_attend/Firebase/Controller.java
+++ EzAttend/app/src/main/java/edu/pace/cs389s2019team5/ez_attend/Firebase/Controller.java
@@ -27,7 +27,8 @@ public class Controller {
     /**
      * Creates a new class session on firebase so that the class attendance can begin
      * getting recorded. It also updates the most recent field of the class
-     * @param classId the id of the class that we wish to create a new session for
+     *
+     * @param classId         the id of the class that we wish to create a new session for
      * @param successListener what should be done when the creation of the class session was successful.
      *                        A class session will be passed which can be used to get the id and timestamp
      * @param failureListener the callback if there is some sort of error when attempting to create
@@ -77,7 +78,8 @@ public class Controller {
 
     /**
      * Creates a new class on firebase, with the provided teacher id as the teacher.
-     * @param teacherId the id of the teacher that is creating the class
+     *
+     * @param teacherId       the id of the teacher that is creating the class
      * @param successListener what should be done when the creation of the class session was successful.
      *                        The id for the newly created class will be passed
      * @param failureListener the callback when there is an error creating the class
@@ -107,8 +109,9 @@ public class Controller {
 
     /**
      * Adds a student to a specific class that is given
-     * @param classId the id of the class that the student would like to join
-     * @param studentId the id of the student that would like to join the class
+     *
+     * @param classId         the id of the class that the student would like to join
+     * @param studentId       the id of the student that would like to join the class
      * @param successListener the callback if the student was successfully added to the class
      * @param failureListener the callback if the student couldn't join the class. This could mean
      *                        the class doesn't exist
@@ -128,6 +131,7 @@ public class Controller {
 
     /**
      * Ends the session with the given id
+     *
      * @return
      */
     public boolean endClassSession() {
@@ -138,8 +142,9 @@ public class Controller {
     /**
      * Given the id of the student, should mark present in the most recent class session if the student
      * is not already marked
-     * @param classId the id of the class we wish to be marked present for
-     * @param studentId the id of the student being marked present
+     *
+     * @param classId           the id of the class we wish to be marked present for
+     * @param studentId         the id of the student being marked present
      * @param onSuccessListener the callback if the marking was successful
      * @param onFailureListener the callback if there was an error marking the student present
      */
@@ -214,8 +219,9 @@ public class Controller {
      * As opposed to markPresent which is used by a student to mark themselves present, this adds
      * a new field to the document called teacherTimestamp which marks when the teacher device
      * detected the student.
-     * @param classId the id of the class that the teacher is marking the student present for
-     * @param studentId the id of the student that the teacher wishes to mark present
+     *
+     * @param classId           the id of the class that the teacher is marking the student present for
+     * @param studentId         the id of the student that the teacher wishes to mark present
      * @param onSuccessListener the callback when the student is successfully marked present
      * @param onFailureListener the callback when marking the student present fails
      */
@@ -284,5 +290,60 @@ public class Controller {
                         onFailureListener.onFailure(e);
                     }
                 });
+    }
+
+
+    /**
+     * Used by the teacher to mark a student present. This is specifically used for manual activation.
+     * This allows a teacher to change a student to present in a given class and class session.
+     *
+     * @param classId           the id of the class that the teacher is marking the student present for
+     * @param sessionId         the id of the session that the teacher is marking the student present for
+     * @param studentId         the id of the student that the teacher wishes to mark present
+     * @param onSuccessListener the callback when the student is successfully marked present
+     * @param onFailureListener the callback when marking the student present fails
+     */
+    public void markManualPresent(final String classId,
+                                final String sessionId,
+                                final String studentId,
+                                final OnSuccessListener<Void> onSuccessListener,
+                                final OnFailureListener onFailureListener) {
+
+    }
+
+    /**
+     * Used by the teacher to mark a student late. This is specifically used for manual activation.
+     * This allows a teacher to change a student to late in a given class and class session.
+     *
+     * @param classId           the id of the class that the teacher is marking the student late for
+     * @param sessionId         the id of the session that the teacher is marking the late late for
+     * @param studentId         the id of the student that the teacher wishes to mark present
+     * @param onSuccessListener the callback when the student is successfully marked late
+     * @param onFailureListener the callback when marking the student late fails
+     */
+    public void markManualLate(final String classId,
+                                  final String sessionId,
+                                  final String studentId,
+                                  final OnSuccessListener<Void> onSuccessListener,
+                                  final OnFailureListener onFailureListener) {
+
+    }
+
+    /**
+     * Used by the teacher to mark a student absent. This is specifically used for manual activation.
+     * This allows a teacher to change a student to absent in a given class and class session.
+     *
+     * @param classId           the id of the class that the teacher is marking the student absent for
+     * @param sessionId         the id of the session that the teacher is marking the student absent for
+     * @param studentId         the id of the student that the teacher wishes to mark absent
+     * @param onSuccessListener the callback when the student is successfully marked absent
+     * @param onFailureListener the callback when marking the student absent fails
+     */
+    public void markManualAbsent(final String classId,
+                                  final String sessionId,
+                                  final String studentId,
+                                  final OnSuccessListener<Void> onSuccessListener,
+                                  final OnFailureListener onFailureListener) {
+
     }
 }
